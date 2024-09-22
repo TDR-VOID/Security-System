f262968cfab662aec037115796123f0472e9b101
Tharaka Rupasinghe <140690160+TDR-VOID@users.noreply.github.com>
2024-09-22 11:34:02 +0530

    INT handle:

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,40 @@
 #define PIR_PIN 13
 #define SpeakPin 14
 #define LDR_PIN 34
+#define BUTTON1_PIN 12
+#define BUTTON2_PIN 27
+#define BUTTON3_PIN 33
+#define LED_Task1 32
+#define LED_Task2 26
+#define LED 25
+
+volatile bool  
+ = false;
+volatile bool task2Active = false;
+volatile bool startTasks = false;
+volatile bool bothTasksActive = false;
 
 int LDRValue = 0;
 int k = 1;
+bool in = false;
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
 void Task1code( void * pvParameters );
 void Task2code( void * pvParameters );
+void IRAM_ATTR button1ISR();
+void IRAM_ATTR button2ISR();
+void IRAM_ATTR button3ISR();
 
 String sensorValueString;
 
+
+unsigned long lastDebounceTime = 0;  // To handle button debouncing
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+unsigned long lastDebounceTime3 = 0;
+unsigned long debounceDelay = 50;    // Minimum delay between button presses
 
 void writeFile(fs::FS &fs, const char * path, const char * message, bool append = false){
   Serial.printf("Writing file: %s\n", path);
@@ -47,6 +69,16 @@ void setup(){
   Serial.begin(115200);
   pinMode(PIR_PIN, INPUT_PULLDOWN);
   pinMode(SpeakPin, OUTPUT);
+  pinMode(BUTTON1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON2_PIN, INPUT_PULLUP);
+  pinMode(BUTTON3_PIN, INPUT_PULLUP);
+  pinMode(LED_Task1, OUTPUT);
+  pinMode(LED_Task2, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON1_PIN), button1ISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON2_PIN), button2ISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON3_PIN), button3ISR, FALLING);
 
 
   xTaskCreatePinnedToCore(
@@ -93,11 +125,67 @@ void setup(){
 
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
   Serial.printf("SD Card Size: %lluMB\n", cardSize);
+  if (cardType != CARD_SD) {
+    digitalWrite(LED, HIGH);
+    in = true;
+  }
+  
 
 }
 
+// ISR function for button press
+// ISR function for button press
+void IRAM_ATTR button1ISR() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastDebounceTime1) > debounceDelay) {
+    task1Active = true;
+    task2Active = false;
+    // bothTasksActive = false;  // Disable both-tasks mode
+    lastDebounceTime1 = currentTime;
+  }
+}
+void IRAM_ATTR button2ISR() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastDebounceTime2) > debounceDelay) {
+    task1Active = false;
+    task2Active = true;
+    // bothTasksActive = false;  // Disable both-tasks mode
+    lastDebounceTime2 = currentTime;
+  }
+}
+
+
+void IRAM_ATTR button3ISR() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastDebounceTime3) > debounceDelay) {
+    bothTasksActive = !bothTasksActive;  // Toggle both-tasks mode
+    // task1Active = bothTasksActive;
+    // task2Active = bothTasksActive;
+    if (bothTasksActive) {
+      task1Active = true;
+      task2Active = true;
+    } else {
+      task1Active = false;
+      task2Active = false;
+    }
+
+    lastDebounceTime3 = currentTime;
+  }
+}
+
+
 void Task1code(void * pvParameters) {
   while (1) {  // Run indefinitely
+    if (task1Active) {
+      if (in == false)
+      {
+        digitalWrite(LED, HIGH);
+        delay(1000);
+        digitalWrite(LED, LOW);
+        delay(1000);
+      }
+      
+    digitalWrite(LED_Task1, HIGH);
 
       // Get the time since the program started
     unsigned long currentMillis = millis();
@@ -118,17 +206,35 @@ void Task1code(void * pvParameters) {
     
     delay(1000); // Log every second
     }
+    else {
+      digitalWrite(LED_Task1, LOW);
+      Serial.println("Task1 Not Active\n"); 
+      delay(1500);
   }
-  
+}
+}
+
 
 void Task2code(void * pvParameters) {
   while (1) {  // Run indefinitely
+    if (task2Active) {
+
+      if (in == false)
+      {
+        digitalWrite(LED, HIGH);
+        delay(1000);
+        digitalWrite(LED, LOW);
+        delay(1000);
+      }
+      
+  digitalWrite(LED_Task2, HIGH);
+    
   // Serial.println("Task2 Running");
   LDRValue = analogRead(LDR_PIN);
   String LDRValueString = String(LDRValue);
   int sensorValue = digitalRead(PIR_PIN);  // Read the PIR sensor value (HIGH or LOW)
 
-  if (k == 0 || LDRValue > 500) {
+  if (k == 0 || LDRValue > 2300) {
     for (int i = 0; i < 10; i++){
     digitalWrite(SpeakPin, HIGH);
     delay(200);
@@ -137,8 +243,6 @@ void Task2code(void * pvParameters) {
     }
     k = 1;
   }
-  
-  
   
   if (sensorValue == HIGH) {
     Serial.println("Motion Detected! LDR Value: "+LDRValueString);
@@ -161,7 +265,16 @@ void Task2code(void * pvParameters) {
   
   delay(100);  // Wait for 1 second before the next reading
   }
+  else {
+    digitalWrite(LED_Task2, LOW);
+    Serial.println("Task2 Not Active\n");
+    delay(1500);
+  }
+  }
 }
+
+
+
 
 void loop(){
   
